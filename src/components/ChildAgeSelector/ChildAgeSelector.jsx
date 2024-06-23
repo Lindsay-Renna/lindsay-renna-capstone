16d670004ef580677196957d227d3c6b2a0794f1
Lindsay Renna <lrenna.oct@gmail.com>
2024-06-22 23:01:32 -0400

    selection buttons created

--- src/components/ChildAgeSelector/ChildAgeSelector.jsx
+++ src/components/ChildAgeSelector/ChildAgeSelector.jsx
@@ -2,10 +2,27 @@ import "./ChildAgeSelector.scss";
 
 function ChildAgeSelector({ data }) {
 	const { numKids, childAges } = data;
+
 	console.log(numKids);
 	return (
 		<div>
 			<h3>How old are the kids?</h3>
+			{[...Array(numKids)].map((_, index) => {
+				return (
+					<div key={index} className="selection-box">
+						<label>
+							{`Child ${index + 1}`}
+							<select name="childAge">
+								{Array.from({ length: 12 }).map((_, i) => (
+									<option key={i} value={i + 1}>
+										{i + 1}
+									</option>
+								))}
+							</select>
+						</label>
+					</div>
+				);
+			})}
 		</div>
 	);
 }
