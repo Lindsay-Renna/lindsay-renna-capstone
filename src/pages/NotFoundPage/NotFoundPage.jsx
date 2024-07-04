1bcf45c4e11b3951008c2802e268ea4047cd3431
Lindsay Renna <lrenna.oct@gmail.com>
2024-07-03 22:31:37 -0400

    404-page done

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -2,8 +2,19 @@ import "./NotFoundPage.scss";
 
 function NotFoundPage() {
 	return (
-		<div>
-			<h1>Something went wrong, sorry!</h1>
+		<div className="not-found">
+			<div className="not-found__image-wrapper">
+				<img
+					src="/src/assets/404-image.png"
+					alt="404 not found"
+					className="not-found__image"
+				/>
+				<p className="not-found__text">
+					Oops! This page doesn't exist. The URL may be misspelled or the page
+					you're looking for is no longer available.
+				</p>
+				<p className="not-found__text">How you got here is a mystery.</p>
+			</div>
 		</div>
 	);
 }
