2ec0c06ec8ab2436592548f3b7fc7ef5f2061d9b
Lindsay Renna <lrenna.oct@gmail.com>
2024-07-11 12:40:32 -0400

    working on swiper - image sizing conforming

--- src/pages/PopularPage/PopularPage.jsx
+++ src/pages/PopularPage/PopularPage.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { EffectCoverflow, Pagination } from "swiper/modules";
 import {
@@ -11,6 +12,7 @@ import {
 	MOVIE_BASE_IMAGE_URL,
 	MOVIE_BASE_URL,
 } from "../../utilities/movie-api.js";
+import { VG_BASE_URL } from "../../utilities/videogame-api.js";
 
 function PopularPage() {
 	const [movieResults, setMovieResults] = useState([]);
@@ -69,10 +71,10 @@ function PopularPage() {
 
 	return (
 		<div className="popular">
-			<div className="pop-movies">
+			<div className="popular__movies">
 				<h2>Top 10 movies for families</h2>
 				{loading ? (
-					<div className="loading">
+					<div className="popular__loading">
 						<p>loading...</p>
 					</div>
 				) : movieResults.length ? (
@@ -81,7 +83,18 @@ function PopularPage() {
 							effect={"coverflow"}
 							grabCursor={true}
 							centeredSlides={true}
-							slidesPerView={"3"}
+							slidesPerView={"auto"}
+							breakpoints={{
+								300: { slidesPerView: 1 },
+
+								400: {
+									slidesPerView: 2,
+								},
+								// when window width is >= 1024px
+								1024: {
+									slidesPerView: 3,
+								},
+							}}
 							coverflowEffect={{
 								rotate: 40,
 								stretch: 0,
@@ -89,14 +102,13 @@ function PopularPage() {
 								modifier: 1,
 								slideShadows: false,
 							}}
-							pagination={true}
+							pagination={{ clickable: true }}
 							modules={[EffectCoverflow, Pagination]}
 							className="mySwiper"
 						>
 							{movieResults.map((movie) => (
-								<SwiperSlide key={movie.id} className="swiper-slide-custom">
+								<SwiperSlide key={movie.id}>
 									<img
-										className="swiper-slide-img"
 										src={`${MOVIE_BASE_IMAGE_URL}${movie.poster_path}`}
 										alt={movie.title}
 									/>
