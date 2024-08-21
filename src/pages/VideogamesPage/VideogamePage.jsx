ae9ad896e036d51076b59de404fa28dabc212f33
Lindsay Renna <lrenna.oct@gmail.com>
2024-08-21 09:30:36 -0400

    added profile population to videogame page

--- src/pages/VideogamesPage/VideogamePage.jsx
+++ src/pages/VideogamesPage/VideogamePage.jsx
@@ -1,6 +1,6 @@
 import "./VideogamePage.scss";
-
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Carousel from "../../components/Carousel/Carousel";
 import PersonSelection from "../../components/PersonSelection/PersonSelection";
@@ -9,7 +9,10 @@ import GamingSystems from "../../components/GamingSystems/GamingSystems";
 import VideogameGenre from "../../components/VideogameGenre/VideogameGenre";
 import VideogameTagSelection from "../../components/VideogameTagSelection/VideogameTagSelection";
 
-function VideogamePage() {
+const SERVER_URL = import.meta.env.VITE_APP_SERVER_URL;
+
+function VideogamePage({ isLoggedIn }) {
+	const [familyProfiles, setFamilyProfiles] = useState([]);
 	const [data, setData] = useState({
 		numKids: 0,
 		numAdults: 0,
@@ -19,6 +22,54 @@ function VideogamePage() {
 		cooperative: true,
 		splitScreen: true,
 	});
+
+	useEffect(() => {
+		if (isLoggedIn) {
+			const user_id = localStorage.getItem("user_id");
+
+			const getFamilyProfiles = async (id) => {
+				try {
+					const { data } = await axios.get(`${SERVER_URL}/user/${id}/family`);
+					const profiles = data.sort((a, b) => {
+						if (a.age >= 1 && a.age <= 13 && b.age === 0) {
+							return -1;
+						} else if (a.age === 0 && b.age >= 1 && b.age <= 13) {
+							return 1;
+						} else {
+							return a.age - b.age;
+						}
+					});
+					setFamilyProfiles(profiles);
+
+					let numAdults = 0;
+					let numKids = 0;
+					let childAges = [];
+
+					data.forEach((profile) => {
+						if (profile.age === 0) {
+							numAdults += 1;
+						} else if (profile.age > 0) {
+							numKids += 1;
+							childAges.push(profile.age);
+						}
+					});
+
+					setData((prevData) => ({
+						...prevData,
+						numKids,
+						numAdults,
+						childAges,
+					}));
+				} catch (error) {
+					console.log("Error fetching family profiles:", error);
+				}
+			};
+
+			if (user_id) {
+				getFamilyProfiles(user_id);
+			}
+		}
+	}, [isLoggedIn]);
 
 	const navigate = useNavigate();
 
@@ -32,10 +83,16 @@ function VideogamePage() {
 	};
 
 	const removeKid = () => {
-		setData((prevData) => ({
-			...prevData,
-			numKids: Math.max(0, prevData.numKids - 1),
-		}));
+		setData((prevData) => {
+			const newChildAges = [...prevData.childAges];
+			newChildAges.pop();
+
+			return {
+				...prevData,
+				numKids: Math.max(0, prevData.numKids - 1),
+				childAges: newChildAges,
+			};
+		});
 	};
 
 	const addAdult = () => {
@@ -58,7 +115,7 @@ function VideogamePage() {
 		const value = event.target.value;
 		setData((prevData) => {
 			const newChildAges = [...prevData.childAges];
-			newChildAges[index] = value;
+			newChildAges[index] = parseInt(value);
 			return { ...prevData, childAges: newChildAges };
 		});
 	};
@@ -96,6 +153,8 @@ function VideogamePage() {
 		navigate("/videogames/results", { state: { data } });
 	};
 
+	console.log(data);
+
 	return (
 		<div id="videogame-selection">
 			<main className="videogame-main">
@@ -111,8 +170,14 @@ function VideogamePage() {
 					handleToggle={handleToggle}
 					handleSubmit={handleSubmit}
 				>
-					<PersonSelection />
-					<ChildAgeSelector />
+					<PersonSelection
+						isLoggedIn={isLoggedIn}
+						familyProfiles={familyProfiles}
+					/>
+					<ChildAgeSelector
+						isLoggedIn={isLoggedIn}
+						familyProfiles={familyProfiles}
+					/>
 					<GamingSystems />
 					<VideogameGenre />
 					<VideogameTagSelection />
